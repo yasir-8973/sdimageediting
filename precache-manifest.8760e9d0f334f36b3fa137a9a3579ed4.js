e138210004a6b20fc40c8656d2df57eacd081b07
Yasir <yaseer@nanosoftengineers.com>
2020-12-19 16:40:51 +0530

    Websote

---
+++ precache-manifest.8760e9d0f334f36b3fa137a9a3579ed4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bb11bf3e5ea39b083061ea00fafe8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc10ccbe066c757b9d47",
+    "url": "/static/css/main.e03c9d1e.chunk.css"
+  },
+  {
+    "revision": "e083190591847e311368",
+    "url": "/static/js/2.54ff04f9.chunk.js"
+  },
+  {
+    "revision": "0d0aa8be310fd066973937b6367f2f73",
+    "url": "/static/js/2.54ff04f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc10ccbe066c757b9d47",
+    "url": "/static/js/main.b948bfce.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "f62de5c84482a92225e8141d906a9e4a",
+    "url": "/static/media/HDR1.f62de5c8.jpeg"
+  },
+  {
+    "revision": "afe7c98899aeb0d56fcb23ee9679cc17",
+    "url": "/static/media/HDR2.afe7c988.jpeg"
+  },
+  {
+    "revision": "0539d648cd180d623b84346a32f04856",
+    "url": "/static/media/HDR3.0539d648.jpeg"
+  },
+  {
+    "revision": "3b132ba29f4c730c2c63372f9078135b",
+    "url": "/static/media/SKY1.3b132ba2.jpeg"
+  },
+  {
+    "revision": "aaeb402dcb5f91ac2c2a8983deda32a9",
+    "url": "/static/media/SKY2.aaeb402d.jpeg"
+  },
+  {
+    "revision": "c310171c113c7f140099e5d5f803ab8e",
+    "url": "/static/media/SKY3.c310171c.jpeg"
+  },
+  {
+    "revision": "109872ba2e893eb9070e3e45de3dcf40",
+    "url": "/static/media/WEDDING1.109872ba.jpeg"
+  },
+  {
+    "revision": "0d833d9f24c0e0a0865d112c296762d4",
+    "url": "/static/media/WEDDING2.0d833d9f.jpeg"
+  }
+]);
